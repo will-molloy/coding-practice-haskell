b0871757684089e1a81a3ea2589b86b6a5bdb9ef
Will Molloy <willjoemolloy@gmail.com>
2019-12-21 19:55:48 +1300

    projecteuler problem5

---
+++ projecteuler/main/problem5.hs
@@ -0,0 +1,16 @@
+-- https://projecteuler.net/problem=5
+
+problem5 :: Int -> Int
+problem5 n = head ([x | x <- [1..], divisibleByAll x [1..n]])
+
+divisibleByAll :: Int -> [Int] -> Bool
+divisibleByAll n list = forAll divides n list
+    where divides a b = a `mod` b == 0
+    
+forAll :: (Int -> Int -> Bool) -> Int -> [Int] -> Bool
+forAll p n [] = True
+forAll p n list = p n (head list) && forAll p n (tail list)
+
+main :: IO()
+main = print (problem5 10) -- 2520
+-- main = print (problem5 20) -- 232792560
