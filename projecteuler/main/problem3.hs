c8315a59919e24e05d308559e7f608acfd8af22a
Will Molloy <willjoemolloy@gmail.com>
2019-12-21 20:25:09 +1300

    refactor projecteuler problem3 to use exists quantification in isPrime implementation

--- projecteuler/main/problem3.hs
+++ projecteuler/main/problem3.hs
@@ -7,12 +7,16 @@ reversePrimeFactors :: Int -> [Int]
 reversePrimeFactors n = [x | x <- reverse([1..(isqrt n)]), n `mod` x == 0 && isPrime x]
 
 isPrime :: Int -> Bool
-isPrime 1 = False
-isPrime n = null [x | x <- [2..(isqrt n)], n `mod ` x == 0]
+isPrime n = not (exists divides n [2..(isqrt n)])
+    where divides a b = a `mod` b == 0 
+
+exists :: (Int -> Int -> Bool) -> Int -> [Int] -> Bool
+exists p n [] = False
+exists p n (head:tail) = p n head || exists p n tail
 
 isqrt :: Int -> Int
 isqrt = floor . sqrt . fromIntegral
 
-
+main :: IO()
 -- main = print (problem3 13195) -- 29
 main = print (problem3 600851475143) -- 6857
