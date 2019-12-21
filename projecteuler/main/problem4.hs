5d2ec7aa723a3bbd8ed3c5ceeac1819ec4da81de
Will Molloy <willjoemolloy@gmail.com>
2019-12-21 18:55:42 +1300

    projecteuler problem4

---
+++ projecteuler/main/problem4.hs
@@ -0,0 +1,18 @@
+-- https://projecteuler.net/problem=4
+
+problem4 :: Int -> Int
+problem4 n = maximum (palindromes n)
+
+palindromes :: Int -> [Int]
+palindromes n = [(x*y) | x <- list, y <- list, isPalindrome (x*y)]
+    where list = [(10^(n-1))..((10^n)-1)]
+
+isPalindrome :: Int -> Bool
+isPalindrome n = n == reverseInt n
+
+reverseInt :: Int -> Int
+reverseInt = read . reverse . show
+
+
+-- main = print (problem4 2) -- 9009
+main = print (problem4 3) -- 906609
