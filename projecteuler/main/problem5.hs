cb0bfda39e810594ebab5d4b8a7ebf803197c2d0
Will Molloy <willjoemolloy@gmail.com>
2019-12-21 20:15:02 +1300

    refactor projecteuler problem5 to deconstruct list into head/tail via. pattern matching

--- projecteuler/main/problem5.hs
+++ projecteuler/main/problem5.hs
@@ -1,7 +1,7 @@
 -- https://projecteuler.net/problem=5
 
 problem5 :: Int -> Int
-problem5 n = head ([x | x <- [1..], divisibleByAll x [1..n]])
+problem5 n = head [x | x <- [1..], divisibleByAll x [1..n]]
 
 divisibleByAll :: Int -> [Int] -> Bool
 divisibleByAll n list = forAll divides n list
@@ -9,7 +9,7 @@ divisibleByAll n list = forAll divides n list
     
 forAll :: (Int -> Int -> Bool) -> Int -> [Int] -> Bool
 forAll p n [] = True
-forAll p n list = p n (head list) && forAll p n (tail list)
+forAll p n (head:tail) = p n head && forAll p n tail
 
 main :: IO()
 main = print (problem5 10) -- 2520
