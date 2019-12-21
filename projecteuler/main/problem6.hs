8b009fc9c7dd1fe06dcc29af7ae2885a63295aa1
Will Molloy <willjoemolloy@gmail.com>
2019-12-21 20:03:07 +1300

    projecteuler problem6

---
+++ projecteuler/main/problem6.hs
@@ -0,0 +1,14 @@
+-- https://projecteuler.net/problem=6
+
+problem6 :: Int -> Int
+problem6 n = squareOfSum n - sumOfSquares n
+
+sumOfSquares :: Int -> Int
+sumOfSquares n = sum [x^2 | x <- [1..n]]
+
+squareOfSum :: Int -> Int
+squareOfSum n = (sum [1..n])^2
+
+main :: IO()
+-- main = print (problem6 10) -- 2640
+main = print (problem6 100) -- 25164150
